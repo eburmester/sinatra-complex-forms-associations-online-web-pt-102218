8fe1924e12d3d6427ee37f2a6442edf7a054a6ce
Elyse Burmester <burmesterelyse@gmail.com>
2019-03-13 16:03:52 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,14 +11,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-    
     @owner = Owner.create(params[:owner])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
-    @owner.save
-    redirect to "owners/#{@owner.id}"
+  if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
   end
+  redirect "owners/#{@owner.id}"
+end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
